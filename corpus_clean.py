19e14c7d442ff9d43b2ac96a4b86c3b9b9ac5294
qaiser21 <qaiseralam21@gmail.com>
2018-08-28 14:06:09 +0530

    Add files via upload

---
+++ corpus_clean.py
@@ -0,0 +1,91 @@
+ 
+
+import nltk
+from nltk.corpus import stopwords as sw
+from nltk.corpus import wordnet as wn
+from nltk import wordpunct_tokenize
+from nltk import WordNetLemmatizer
+from nltk import sent_tokenize
+from nltk import pos_tag
+import string
+from sklearn.base import BaseEstimator, TransformerMixin
+import re
+from string import punctuation
+
+
+
+class NLTKPreprocessor(BaseEstimator, TransformerMixin):
+
+    def __init__(self, stopwords=None, punct=None,
+                 lower=True, strip=True):
+        self.lower      = lower
+        self.strip      = strip
+        self.stopwords  = stopwords or set(sw.words('english'))
+        self.punct      = punct or set(string.punctuation)
+        self.lemmatizer = WordNetLemmatizer()
+
+    def fit(self, X, y=None):
+        return self
+
+    def inverse_transform(self, X):
+        return [" ".join(doc) for doc in X]
+
+    def transform(self, X):
+        return [
+            list(self.tokenize(doc)) for doc in X
+        ]
+
+    def tokenize(self, document):
+        # Break the document into sentences
+        for sent in sent_tokenize(document):
+            # Break the sentence into part of speech tagged tokens
+            for token, tag in pos_tag(wordpunct_tokenize(sent)):
+                # Apply preprocessing to the token
+                token = token.lower() if self.lower else token
+                token = token.strip() if self.strip else token
+                token = token.strip('_') if self.strip else token
+                token = token.strip('*') if self.strip else token
+
+                # If stopword, ignore token and continue
+                if token in self.stopwords:
+                    continue
+
+                # If punctuation, ignore token and continue
+                if all(char in self.punct for char in token):
+                    continue
+
+                # Lemmatize the token and yield
+                lemma = self.lemmatize(token, tag)
+                yield ''.join(lemma)
+
+    def lemmatize(self, token, tag):
+        tag = {
+            'N': wn.NOUN,
+            'V': wn.VERB,
+            'R': wn.ADV,
+            'J': wn.ADJ
+        }.get(tag[0], wn.NOUN)
+
+        return self.lemmatizer.lemmatize(token, tag)
+
+
+
+
+ # for extracting content of resume
+
+
+
+def read_csv(filename):
+	text  = textract.process(filename)
+ 	return text.decode('utf-8')
+
+
+
+
+def resume_corpus(data_science):
+	resume_corpus = []
+	for cv in data_science:
+		resume_document=read_cv(cv)
+        resume_corpus.append(resume_document)
+    return corpus
+
